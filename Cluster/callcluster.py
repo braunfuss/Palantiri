6fdc1dbbfb35093bd151970337c1b424147ea16c
braunfuss <asteinbe@gfz-potsdam.de>
2019-04-05 15:16:21 +0200

    pep8

--- Cluster/callcluster.py
+++ Cluster/callcluster.py
@@ -1,48 +1,42 @@
-
 import os
 import sys
-
-# add local directories to import path
-
 sys.path.append('../tools/')
 sys.path.append('../Common/')
 
 from optparse import OptionParser
 
-import config       #      Import from tools
-import Globals      #      Import from Common
+import config
+import Globals
 
 parser = OptionParser(usage="%prog -f eventpath ")
 parser.add_option("-f", "--evpath", type="string", dest="evpath", help="evpath")
 
 (options, args) = parser.parse_args()
 
-# -------------------------------------------------------------------------------------------------
 
 def init():
 
-    C  = config.Config(options.evpath)
+    C = config.Config(options.evpath)
     Config = C.parseConfig('config')
 
     tests = int(Config['runs'])
-    #tests = 1
-#   cmd   =('%s cluster2.py -f %s')%(sys.executable,options.evpath)
 
-    at   = os.path.join(os.getcwd(), 'cluster2.py')
-    cmd  = sys.executable + ' ' + at + ' -f '+ options.evpath
+    at = os.path.join(os.getcwd(), 'cluster2.py')
+    cmd = sys.executable + ' ' + at + ' -f '+ options.evpath
     print('cmd = ', cmd)
 
     for i in xrange(tests):
-        print('RUN: ',i)
+        print('RUN: ', i)
         os.system(cmd)
 
-    cmd =('%s evaluateCluster.py -f %s') %(sys.executable,os.path.join(options.evpath,'cluster'))
+    cmd = ('%s evaluateCluster.py -f %s') % (sys.executable,
+                                             os.path.join(options.evpath,
+                                                          'cluster'))
 
     print(cmd)
     print(os.getcwd())
     os.system(cmd)
 
-# -------------------------------------------------------------------------------------------------
 
 if __name__ == "__main__":
 
