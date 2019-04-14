0a2391d1619b09105115822b54803836a1da9c1c
asteinbe <asteinbe@gfz-potsdam.de>
2019-04-14 12:23:09 +0200

    pep8, python3

--- Cluster/evaluateCluster.py
+++ Cluster/evaluateCluster.py
@@ -1,8 +1,6 @@
-
 import os
 import sys
 
-# add local directories to import path
 
 sys.path.append('../tools/')
 sys.path.append('../Common/')
@@ -12,76 +10,75 @@ import logging
 import shutil
 import fnmatch
 
-#       Import from common
-
 import  Basic
 import  Globals
 import  Logfile
-from    ObspyFkt   import loc2degrees, obs_kilometer2degrees
+from ObspyFkt import loc2degrees, obs_kilometer2degrees
 
-from config     import Config             #       Import from Tools
-from cluster2   import Centroid,Station   #       Import from Cluster
-from ConfigFile import ConfigObj          #       Import from common
+from config import Config
+from cluster2 import Centroid,Station
+from ConfigFile import ConfigObj
 
 parser = OptionParser(usage="%prog -f eventpath ")
 parser.add_option("-f", "--evpath", type="string", dest="evpath", help="evpath")
 
 (options, args) = parser.parse_args()
 
-# -------------------------------------------------------------------------------------------------
 
 class Result(object):
 
-    def __init__(self,meanvalue,minvalue,centroidcount,usedstationcount,path):
+    def __init__(self, meanvalue, minvalue, centroidcount, usedstationcount,
+                 path):
 
-        self.meanvalue= meanvalue
-        self.minvalue= minvalue
-        self.centroidcount= centroidcount
+        self.meanvalue = meanvalue
+        self.minvalue = minvalue
+        self.centroidcount = centroidcount
         self.usedstationcount = usedstationcount
-        self.path= path
+        self.path = path
 
-# -------------------------------------------------------------------------------------------------
 
 class BestSolution(object):
 
-    def __init__(self,station,cluster,lat,lon):
+    def __init__(self, station, cluster, lat, lon):
 
         self.station = station
         self.cluster = cluster
         self.lat = lat
         self.lon = lon
 
-# -------------------------------------------------------------------------------------------------
 
 def getStatistics(clusterresults):
-    p= os.path.join(options.evpath,'stat.dat')
-    fobjstat = open(p,'w')
-    resDict  = {}
-    for root,dirs,files in os.walk(clusterresults):
+    p = os.path.join(options.evpath, 'stat.dat')
+    fobjstat = open(p, 'w')
+    resDict = {}
+    for root, dirs, files in os.walk(clusterresults):
         for i in files:
             if i == 'event.statistic':
-                fname  = os.path.join(root,i)
-                bspath = os.path.join('/',*fname.split('/')[:-1])
-                fobj   = open(fname,'r')
+                fname = os.path.join(root, i)
+                bspath = os.path.join('/', *fname.split('/')[:-1])
+                fobj = open(fname, 'r')
 
                 for line in fobj:
                     line = line.split()
-                    resDict[fname] = Result(line[0],line[1],line[2],line[3],bspath)
-                    fobjstat.write(('%s:  %s  %s  %s  %s\n')%(fname,line[0],line[1],line[2],line[3]))
+                    resDict[fname] = Result(line[0], line[1], line[2], line[3],
+                                            bspath)
+                    fobjstat.write(('%s:  %s  %s  %s  %s\n') % (fname, line[0],
+                                                                line[1],
+                                                                line[2],
+                                                                line[3]))
 
                 fobj.close()
-
 
     fobjstat.close()
     return resDict
 
-# -------------------------------------------------------------------------------------------------
 
 def getBestSolution(resultDictionary):
 
     bestsolution = -100000
 
     for i in resultDictionary.keys():
+        resultDictionary[i].meanvalue = float(resultDictionary[i].meanvalue)
         if bestsolution < resultDictionary[i].meanvalue:
             bestsolution = resultDictionary[i].meanvalue
 
@@ -91,48 +88,48 @@ def getBestSolution(resultDictionary):
             L.append(resultDictionary[j])
 
     return L[0]
-# -------------------------------------------------------------------------------------------------
+
 
 def copyCluster2EventConfig(ClusterDict, evpath):
 
-    epath = os.path.join('/',*evpath.split('/')[:-1])
-    t= epath.split(os.path.sep)[-1]
+    epath = os.path.join('/', *evpath.split('/')[:-1])
+    t = epath.split(os.path.sep)[-1]
     fname = t+'.config'
-    fullfname = os.path.join(epath,fname)
-    L= []
-    fobj  = open(fullfname,'r')
+    fullfname = os.path.join(epath, fname)
+    L = []
+    fobj = open(fullfname, 'r')
 
-    for index,line in enumerate(fobj):
+    for index, line in enumerate(fobj):
         L.append(line)
 
-        if fnmatch.fnmatch(line,'*array parameter*'):
+        if fnmatch.fnmatch(line, '*array parameter*'):
             firstend = index+1
 
-        if fnmatch.fnmatch(line,'*beamforming method*'):
+        if fnmatch.fnmatch(line, '*beamforming method*'):
             secondbegin = index
 
     fobj.close()
 
-    Confpart1 = L [:firstend]
-    Confpart2 = L [secondbegin:]
+    Confpart1 = L[:firstend]
+    Confpart2 = L[secondbegin:]
 
-    fobj = open(fullfname,'w')
+    fobj = open(fullfname, 'w')
     fobj.write(''.join(Confpart1))
-    nlist=''
+    nlist = ''
 
     for i in ClusterDict.keys():
         if len(ClusterDict[i]) > 0:
             nlist += 'r'+str(i)+','
 
-    fobj.write(('networks=%s\n')%(nlist[:-1]))
+    fobj.write(('networks=%s\n') % (nlist[:-1]))
     fobj.write('\n')
 
     for i in ClusterDict.keys():
         if len(ClusterDict[i]) > 0:
             aname = 'r'+str(i)
-            fobj.write(('%s=%s\n')%(aname,ClusterDict[i][:-1]))
-            fobj.write(('%srefstation=\n')%(aname))
-            fobj.write(('%sphase=P\n')%(aname))
+            fobj.write(('%s=%s\n') % (aname, ClusterDict[i][:-1]))
+            fobj.write(('%srefstation=\n') %( aname))
+            fobj.write(('%sphase=P\n') % (aname))
 
     fobj.write('\n')
     fobj.write(''.join(Confpart2))
@@ -160,7 +157,7 @@ def printBestSolution(solution):
     C = {}
     fobjarrays = open(os.path.join(os.path.join('/',*solution.path.split('/')[:-2]),'arraycluster.dat'),'w')
 
-    for i in xrange(int(maxline)+1):
+    for i in range(int(maxline)+1):
         array = ''
 
         for j in L:
@@ -178,7 +175,6 @@ def printBestSolution(solution):
 
 def copyAndShowBestSolution(solution):
 
-    dest = solution.path
     src = os.path.join('/', *solution.path.split('/')[:-4])
     src = os.path.join(src, 'skeleton', 'clusterplot.sh')
 
