fdaa401a8373ef345d973debc935bc1b1f9e9525
asteinbe <asteinbe@gfz-potsdam.de>
2019-04-26 16:33:00 +0200

    bugfix for waveform naming

--- Process/waveform.py
+++ Process/waveform.py
@@ -86,7 +86,7 @@ def readWaveformsPyrocko(stationlist, w, EventPath, Origin, desired):
             for tr in traces:
                 tr_name = str(tr.network+'.'+tr.station+'.'+tr.location
                               + '.' + tr.channel[:3])
-                if tr_name == str(il) and tr.channel[-1] == desired:
+                if (tr_name == str(il) and tr.channel[-1] == desired) or (tr_name == str(il)[:-2] and tr.channel[-1] == desired):
                     st = obspy.Stream()
                     es = obspy_compat.to_obspy_trace(tr)
                     st.extend([es])
@@ -117,7 +117,7 @@ def readWaveformsPyrocko_restituted(stationlist, w, EventPath, Origin, desired):
         for il in stationlist:
                 tr_name = str(tr.network+'.'+tr.station+'.'+tr.location
                               + '.' + tr.channel[:3])
-                if tr_name == str(il) and tr.channel[-1] == desired:
+                if (tr_name == str(il) and tr.channel[-1] == desired) or (tr_name == str(il)[:-2] and tr.channel[-1] == desired):
                     st = obspy.Stream()
                     es = obspy_compat.to_obspy_trace(tr)
                     st.extend([es])
